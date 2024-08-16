920d9601b77692ad5121a304df2426d30a1a18cb
Sagnik0684 <sagnik0684@gmail.com>
2024-08-16 18:12:54 +0530

    Updated Files for proper functioning.

--- customer/admin.py
+++ customer/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from .models import Customer
-
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email') 
-    search_fields = ('name', 'email') 
--- insurance/admin.py
+++ insurance/admin.py
@@ -1,11 +1,1 @@
 from django.contrib import admin
-from .models import Policy, Category
-
-@admin.register(Policy)
-class PolicyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'policy_name', 'category', 'status') 
-    list_filter = ('status', 'category') 
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name') 
